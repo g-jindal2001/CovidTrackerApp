e180c227f3a77e449e264c0c2c926add9395637e
g-jindal2001 <gj.address2001@gmail.com>
2021-04-10 18:55:46 +0530

    Added Functionality to see Covid-19 stats for a particular country

--- lib/screens/country.dart
+++ lib/screens/country.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/data.dart';
@@ -17,35 +18,65 @@ class _CountryState extends State<Country> {
 
   @override
   void didChangeDependencies() {
-    if(_isInit) {
+    if (_isInit) {
       final countryName = ModalRoute.of(context).settings.arguments as String;
       try {
-        Provider.of<Data>(context, listen: false).loadAndUpdateCovidCountryData(countryName);
+        Provider.of<Data>(context, listen: false)
+            .loadAndUpdateCovidCountryData(countryName);
       } catch (error) {
         print(error.toString());
       }
-      
     }
 
     _isInit = false;
     super.didChangeDependencies();
   }
 
-
+  Future<void> _refreshCountryData(BuildContext context, String country) async {
+    await Provider.of<Data>(context, listen: false)
+        .loadAndUpdateCovidCountryData(country);
+  }
 
   @override
   Widget build(BuildContext context) {
     final countryName2 = ModalRoute.of(context).settings.arguments as String;
+    final covidCountryData = Provider.of<Data>(context).data;
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Covid-19 cases in $countryName2'),
       ),
-      body: CardHomePage(
-        'Cases',
-        Icons.insights,
-        '13443435',
-        Colors.lightBlue,
+      body: RefreshIndicator(
+        onRefresh: () => _refreshCountryData(context, countryName2),
+        child: ListView(
+          children: [
+            SizedBox(height:15),
+            CardHomePage(
+              'Cases',
+              Icons.insights,
+              covidCountryData['countryCases'],
+              Colors.lightBlue,
+            ),
+            CardHomePage(
+              'Active Cases',
+              Icons.people,
+              covidCountryData['countryActiveCases'],
+              Colors.orange,
+            ),
+            CardHomePage(
+              'Recovered',
+              Icons.science,
+              covidCountryData['countryRecovered'],
+              Colors.green,
+            ),
+            CardHomePage(
+              'Deaths',
+              MdiIcons.skullCrossbones,
+              covidCountryData['countryDeaths'],
+              Colors.red,
+            ),
+          ],
+        ),
       ),
     );
   }
